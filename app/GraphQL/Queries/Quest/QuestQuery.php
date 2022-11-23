de27d68bc689c3fb8eb941602dcbcbc39e467a84
Jalalnx <jalalnx99@gmail.com>
2022-11-23 21:52:51 +0200

    add Querys

---
+++ app/GraphQL/Queries/Quest/QuestQuery.php
@@ -0,0 +1,43 @@
+<?php
+
+// app/graphql/queries/quest/QuestQuery 
+
+namespace App\GraphQL\Queries\Quest;
+
+use App\Models\Quest;
+use GraphQL\Type\Definition\Type;
+
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class QuestQuery extends Query
+{
+          //query configuration.
+    protected $attributes = [
+        'name' => 'quest',
+    ];
+
+    // declare what type of object this query will return.
+    public function type(): Type
+    {
+        return GraphQL::type('Quest');
+    }
+
+    // declare what arguments this query will accept
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    // bulk of the work – it returns the actual object using eloquent.
+    public function resolve($root, $args)
+    {
+        return Quest::findOrFail($args['id']);
+    }
+}
