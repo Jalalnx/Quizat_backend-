5b7cd71799cd83dbda38463b682b1f38248cb220
Jalalnx <jalalnx99@gmail.com>
2022-11-24 00:49:58 +0200

    modify Auth0  middleware

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Auth0\SDK\JWTVerifier;
+
 
 class CheckAccess
 {
@@ -16,17 +18,49 @@ class CheckAccess
      */
     public function handle(Request $request, Closure $next)
     {
+
+         $Bearer = $request->bearerToken();
+        if ( empty( $accessToken ) ) {
+             abort(401, 'Bearer token missing');   
+          
+        }
+
+
        if (!empty(env('AUTH0_AUDIENCE')) && !empty(env('AUTH0_DOMAIN'))) {
-            $verifier = new JWTVerifier([
-                'valid_audiences' => [env('AUTH0_AUDIENCE')],
-                'authorized_iss' => [env('AUTH0_DOMAIN')],
-                'supported_algs' => ['RS256']
-            ]);
-            $token = $request->bearerToken();
-            $decodedToken = $verifier->verifyAndDecode($token);
-            if (!$decodedToken) {
-                abort(403, 'Access denied');
-            }
+         try {
+        //    $verifier = new JWTVerifier([
+        //         'valid_audiences' => [env('AUTH0_AUDIENCE')],
+        //         'authorized_iss' => [env('AUTH0_DOMAIN')],
+        //         'supported_algs' => ['RS256']
+        //     ]);
+
+        
+        $algorithm = 'RS256';
+
+        if ($token === null) {
+            die('No `token` request parameter.');
+        }
+
+        $token = new \Auth0\SDK\Token([
+        'domain' => $env['AUTH0_DOMAIN'],
+        'clientId' => $env['AUTH0_CLIENT_ID'],
+        'clientSecret' => $env['AUTH0_CLIENT_SECRET'],
+        'tokenAlgorithm' => $algorithm
+        ], $Bearer, \Auth0\SDK\Token::TYPE_ID_TOKEN);
+           
+
+        $token->verify();
+
+        $token->validate();
+
+            // if (!$decodedToken) {
+            //     abort(403, 'Access denied');
+            // };
+        } catch ( \Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 401);
+        }
+
+            
         }
         return $next($request);
     }
