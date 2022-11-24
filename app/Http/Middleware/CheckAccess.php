be282a129e4d59286c7b697e780acf6ed22e1e5b
Jalalnx <jalalnx99@gmail.com>
2022-11-24 03:01:25 +0200

    modify Auth0  middleware handler

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -4,8 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Auth0\SDK\JWTVerifier;
-
+use Illuminate\Support\Str;
+// use Auth0\SDK\JWTVerifier;
+use Auth0\SDK\Auth0;
+use Auth0\SDK\Configuration\SdkConfiguration;
 
 class CheckAccess
 {
@@ -19,49 +21,45 @@ class CheckAccess
     public function handle(Request $request, Closure $next)
     {
 
-         $Bearer = $request->bearerToken();
-        if ( empty( $accessToken ) ) {
-             abort(401, 'Bearer token missing');   
-          
-        }
+        //  $Bearer = $request->header('Authorization', '');  
 
+        //  if ( empty( $Bearer ) )abort(401, 'Bearer token missing'); 
 
-       if (!empty(env('AUTH0_AUDIENCE')) && !empty(env('AUTH0_DOMAIN'))) {
-         try {
-        //    $verifier = new JWTVerifier([
-        //         'valid_audiences' => [env('AUTH0_AUDIENCE')],
-        //         'authorized_iss' => [env('AUTH0_DOMAIN')],
-        //         'supported_algs' => ['RS256']
-        //     ]);
+        //     if (Str::startsWith($Bearer, 'Bearer ')) 
+        //     {            
+        //     $Bearer = Str::substr($Bearer, 7);        
+        //     };
 
         
-        $algorithm = 'RS256';
-
-        if ($token === null) {
-            die('No `token` request parameter.');
-        }
-
-        $token = new \Auth0\SDK\Token([
-        'domain' => $env['AUTH0_DOMAIN'],
-        'clientId' => $env['AUTH0_CLIENT_ID'],
-        'clientSecret' => $env['AUTH0_CLIENT_SECRET'],
-        'tokenAlgorithm' => $algorithm
-        ], $Bearer, \Auth0\SDK\Token::TYPE_ID_TOKEN);
            
 
-        $token->verify();
 
-        $token->validate();
+    //    if (!empty(env('AUTH0_AUDIENCE')) && !empty(env('AUTH0_DOMAIN'))) {
+        //  try {
+        
 
-            // if (!$decodedToken) {
-            //     abort(403, 'Access denied');
-            // };
-        } catch ( \Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 401);
-        }
+        // $configuration = new SdkConfiguration(
+        // domain: 'https://dev-iwack5ph0aok5elz.us.auth0.com/',
+        // clientId:'YTX6wqx7qaEpRCHkhtUkcDi6nV2h0q9G',
+        // clientSecret: 'cxsp2Zim-2r8xEsqfXBKaHOSmh2DvWvtfwyRwoMaiHvKVF_IVpO5vGymvG8IW0f4',
+        // cookieSecret: '2r8xEsqfXBKaHOSmh2DvWvtfwyRwoMaiHvKVF_IVpO5vGymvG8IW0f4'.Str::random(15),
+        // );
+
+        // $token = new \Auth0\SDK\Token($configuration, $Bearer, \Auth0\SDK\Token::TYPE_ID_TOKEN);
+    
+        // $token->verify();
+
+        // $token->validate();
 
             
-        }
+        // } catch ( \Exception $e) {
+
+        //     report($e);
+        //     return  $e;//response()->json(['message' => $e->getMessage()], 401);
+        // }
+
+            
+        // }    
         return $next($request);
     }
 }
